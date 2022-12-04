288e91a7c94413db9237b158594853c5d21c8108
SamuelP21 <samueljzd@gmail.com>
2022-12-04 15:49:32 +0100

    QR

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import qr from './assets/images/image-qr-code.png'
+
+
+const App = () => {
+  return (
+    <main className='bg-blue-50 flex justify-center items-center min-h-screen '>
+        <section className='bg-white w-[400px]  rounded-lg p-5'>
+            <img className=' rounded-lg ' src={qr} alt="" />
+            <p className=' mt-5 text-xl text-blue-900 text-center  '>Imporvide your front-end skills by building projects</p>
+            <p className='mt-5 mb-5  text-base text-gray-400 text-center  '>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </section>
+    </main>
+  )
+}
+
+export default App 
